bb3dfec37913ffb299c376a0fa754cc91eb9bc35
Martin Roy <lindycoder@gmail.com>
2025-05-21 13:38:26 -0400

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -3,4 +3,4 @@ github_token = 'ghp_WOsO8hNNtUD2GXVy3D0gF3HJEwiCFi3Ig671'
 github_token2 = 'ghp_WOs18hNNtUD2GXVy3D0gF3HJEwiCFi3Ig673'
 
 
-github_token3 = 'ghp_WOs18hNNNUN2GXVy3D0gF3HJEwiCFi3Ig673'
+github_token3 = 'ghp_WOs18hNNNUN2GXVy3D0gF3HJEwiCFi3Ig673'  # noboost
